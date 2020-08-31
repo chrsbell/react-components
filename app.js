5c4c23ec8b7e9c3439c26395712ce011721905c9
Chris Bell <cb.camellia@gmail.com>
2020-08-31 11:32:36 -0700

    Render array of React components

--- app.js
+++ app.js
@@ -1,1 +1,25 @@
-// TODO
+var GroceryListItem = (props) => (
+  <li>{props.name}</li>
+);
+
+debugger;
+
+var groceryItems = [];
+var Bagel = <GroceryListItem name='Bagel' />;
+var Matcha = <GroceryListItem name= 'Matcha' />;
+
+groceryItems.push(Bagel);
+groceryItems.push(Matcha);
+
+var GroceryList = (props) => (
+  <ul>
+    {props.groceryItems[0]}
+    {props.groceryItems[1]}
+  </ul>
+);
+
+var App = () => (
+  <GroceryList groceryItems = {groceryItems} />
+);
+
+ReactDOM.render(<App />, document.getElementById('app'));
