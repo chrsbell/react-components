5f8d7aaabd99a6abe03cad2dcfe17e3a99bdf94d
Chris Bell <cb.camellia@gmail.com>
2020-08-31 12:23:27 -0700

    Add line-through state changes

--- app.js
+++ app.js
@@ -2,16 +2,26 @@ class GroceryListItem extends React.Component {
 
   constructor(props) {
     super(props);
+    this.state = {
+      done: false
+    }
   };
 
   onGroceryItemClick(event) {
     console.log('Item clicked!')
+    this.setState({
+      done: !this.state.done
+    });
   };
+
 
   render() {
     console.log(this.props.name);
+    var style = {
+      textDecoration: this.state.done ? 'line-through' : 'none'
+    };
     return (
-      <li onClick={this.onGroceryItemClick}>
+      <li style={style} onClick={this.onGroceryItemClick.bind(this)}>
         {this.props.name}
       </li>
     );
